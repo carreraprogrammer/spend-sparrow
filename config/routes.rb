f9e6a1fa4178de2b4f872852ff96c3afb1984eaa
Daniel <carreraprogrammer@gmail.com>
2023-08-14 18:39:04 -0500

    Personalize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :budgets
-  resources :categories
-  devise_for :users
+  devise_for :users, path: ''
+  resources :categories do
+    resources :budgets
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
